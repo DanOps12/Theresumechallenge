1bb57ee678afe5876504d9135d4f43b431444ea1
Daniel Constantino <danielconstantinouk@gmail.com>
2025-11-11 18:26:38 +0000

    experience page, projects page, skills page, css and javascript

---
+++ script.js
@@ -0,0 +1,1 @@
+<><div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="85822f2c-4544-4eca-9097-7c36fc87bec3" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script></>
